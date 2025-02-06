188131609360b5ae545233acffa7961e64df8c4e
AboubakarJay <aboubakarjay2@hotmail.com>
2025-02-06 15:27:06 +0100

    adding --custom-red

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-blue": "#f0ffff",
+      },
+    },
   },
   plugins: [],
 };
